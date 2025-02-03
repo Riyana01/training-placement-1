cfde918b56ddde568733e6980da935d1452ce9f8
Riyana01 <riyaazee018@gmail.com>
2025-02-03 04:59:07 -0800

    Create generate random num.py

---
+++ 03.02.2025/generate random num.py
@@ -0,0 +1,5 @@
+a = int(input("Enter a lower bound : "))
+b = int(input("Enter an upper bound : "))
+import random
+random_number = random.randint(a,b)
+print("Generated random number is {}".format(random_number))
